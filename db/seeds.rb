ef10a8117f1421e16d3ae9a3147b83a99b7767ff
SamGarner <samuel.w.garner@gmail.com>
2021-01-18 17:27:16 -0600

    Feat: Add Passenger and Booking seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,10 +12,18 @@ Airport.create([{ id: 1, code: 'MDW', city: 'Chicago' }, { id: 2, code: 'DEN', c
                 { id: 5, code: 'IST', city: 'Istanbul, TR' }])
 
 Flight.destroy_all # only since playground app
-Flight.create([{ duration: 439, take_off: '2011-01-05 01:00:00 UTC', departure_id: 1, arrival_id: 3 },
-               { duration: 439, take_off: '2011-01-05 04:00:00 UTC', departure_id: 1, arrival_id: 3 },
-               { duration: 439, take_off: '2011-01-05 07:00:00 UTC', departure_id: 1, arrival_id: 3 },
-               { duration: 154, take_off: '2021-05-05 01:00:00 UTC', departure_id: 3, arrival_id: 5 },
-               { duration: 597, take_off: '2022-09-13 09:22:10 UTC', departure_id: 4, arrival_id: 2 },
-               { duration: 123, take_off: '2020-09-22 18:20:00 UTC', departure_id: 2, arrival_id: 1 },
-               { duration: 692, take_off: '2011-01-05 11:00:00 UTC', departure_id: 1, arrival_id: 5 }])
+Flight.create([{ id: 1, duration: 439, take_off: '2011-01-05 01:00:00 UTC', departure_id: 1, arrival_id: 3 },
+               { id: 2, duration: 439, take_off: '2011-01-05 04:00:00 UTC', departure_id: 1, arrival_id: 3 },
+               { id: 3, duration: 439, take_off: '2011-01-05 07:00:00 UTC', departure_id: 1, arrival_id: 3 },
+               { id: 4, duration: 154, take_off: '2021-05-05 01:00:00 UTC', departure_id: 3, arrival_id: 5 },
+               { id: 5, duration: 597, take_off: '2022-09-13 09:22:10 UTC', departure_id: 4, arrival_id: 2 },
+               { id: 6, duration: 123, take_off: '2020-09-22 18:20:00 UTC', departure_id: 2, arrival_id: 1 },
+               { id: 7, duration: 692, take_off: '2011-01-05 11:00:00 UTC', departure_id: 1, arrival_id: 5 }])
+
+Passenger.destroy_all # only since playground app
+Passenger.create([{ id: 1, name: 'Sam G', email: 'sam@g.com' }, { id: 2, name: 'Woodwick', email: 'wood@wick.com' }])
+
+Booking.destroy_all # only since playground app
+Booking.create([{ passenger_id: 1, flight_id: 1, confirmation_number: 'BD9F3Z' },
+                { passenger_id: 1, flight_id: 6, confirmation_number: '9GPFL0' },
+                { passenger_id: 2, flight_id: 1, confirmation_number: 'LR439C' }])
