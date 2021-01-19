5a2eb7649af1516ea10c3cff8e4a0400e2c4eb88
SamGarner <samuel.w.garner@gmail.com>
2021-01-19 14:21:24 -0600

    Feat: Add #new action to Bookings controller and new.html.erb booking form to created nested passenger info.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,13 @@
+# 
 class BookingsController < ApplicationController
   def new
     if params[:flight_id]
+      @booking_flight_id = params[:flight_id]
+      @booking_flight = Flight.find(params[:flight_id])
+      @booking = Booking.new(flight_id: params[:flight_id]) # nothing saved to DB
+      @passenger_count = params[:passenger_count].to_i
+      @passenger_count.times { @booking.passengers.build } # nothing saved to DB
+
       render :new
     else
       flash[:notice] = 'Please select a flight before proceeding.'
