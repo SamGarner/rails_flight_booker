9085aea06abe4f9b10004a5cbeab95f7fe5474fb
SamGarner <samuel.w.garner@gmail.com>
2021-01-25 19:02:28 -0600

    Feat: Add #confirmation_email and default from: to PassengerMailer.

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,12 @@
 class PassengerMailer < ApplicationMailer
+  default from: 'confirmations@example.com'
+
+  def confirmation_email
+    @passengers = params[:passengers]
+    @confirmation_number = params[:confirmation_number]
+    @passengers.each do |passenger|
+      @passengers = passenger
+      mail(to: passenger.email, subject: 'Your flights been booked!')
+    end
+  end
 end
